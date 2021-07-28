103517e0faec8bbae84e8cf8907a2527c2c7863b
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-27 21:51:40 -0700

    Add solution

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return "<!doctype html><html>Hi! This is the home page. <a href='/hello'>Hello</a></html>"
 
 
 @app.route('/hello')
@@ -35,6 +35,22 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <label for='compliments'>Select a compliment:</label>
+          <select name='compliments' id=compliments'>
+            <option value='awesome'>awesome</option>
+            <option value='terrific'>terrific</option>
+            <option value='neato'>neato</option>
+            <option value='ducky'>ducky</option>
+          <input type="submit" value="Submit">
+        </form>
+        <form action="/superhero">
+          What's your name? <input type="text" name="person">
+          <label for='superhero'>Select a superhero:</label>
+          <select name='superhero' id=superhero'>
+            <option value='ironman'>Iron Man</option>
+            <option value='wonderwoman'>Wonder Woman</option>
+            <option value='batman'>Batman</option>
+            <option value='mrs.incredible'>Mrs. Incredible</option>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,7 +64,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get('compliments')
 
     return f"""
     <!doctype html>
@@ -58,6 +74,25 @@ def greet_person():
       </head>
       <body>
         Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+
+@app.route('/superhero')
+def diss():
+    player = request.args.get("person")
+
+    superhero = request.args.get('superhero')
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Superhero</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're as incredible as {superhero}!
       </body>
     </html>
     """
